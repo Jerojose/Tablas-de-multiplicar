c03c60c27cfe6f588933695b022bcd232907610e
Jerojose <132441170+Jerojose@users.noreply.github.com>
2023-05-03 10:46:48 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+let variable_2 = 0
+let variable_1 = 0
+input.onButtonPressed(Button.A, function () {
+    variable_2 = 2
+    basic.showNumber(variable_2)
+    variable_1 = randint(0, 10)
+    basic.showNumber(variable_1)
+    basic.showNumber(variable_1 * variable_2)
+})
